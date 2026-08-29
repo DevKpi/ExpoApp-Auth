bc506edcde9f94df25865befa330945e97966504
Thiago <thiagobelpo@gmail.com>
2026-08-28 21:14:59 -0300

    feat: agregar archivos base para ConfigScreen y ConfigStyles

---
+++ src/screens/ConfigScreen.js
@@ -0,0 +1,4 @@
+import { Alert, Pressable, ScrollView, Text, View } from 'react-native';
+import { auth } from '../firebase/firebase';
+import { logout } from '../firebase/authService';
+import { styles } from '../styles/ConfigStyles';
